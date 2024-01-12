29c439c920333af37607e4c47edfa3df074dff10
Azhar Khan <119923339+azharkhan93@users.noreply.github.com>
2024-01-12 18:11:22 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule,
+    // NavbarComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    FooterComponent,
+     RouterOutlet,
+    ],
+    //  imports: [
+    //   HomeComponent,
+    // ],
+  // declarations: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  open = false;
+
+  toggleNavbar() {
+    this.open = !this.open;
+  }
+
+}
